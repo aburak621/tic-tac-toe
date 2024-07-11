7b8feb0cf3539f2cc00d12341b5129765765dada
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2024-07-11 13:34:37 +0300

    Implement console version

--- script.js
+++ script.js
@@ -1,52 +1,131 @@
 const Gameboard = (() => {
-  const board = [['', '', ''], ['', '', ''], ['', '', '']];
+  const rows = 3;
+  const cols = 3;
+  let board = [];
 
-  const renderBoard = (domElement) => {
-    const brd = document.createElement('div');
-    brd.classList.add('board');
-
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.row = i.toString();
-        cell.col = j.toString();
-
-        const img = document.createElement('img');
-        cell.appendChild(img);
-
-        brd.appendChild(cell);
+  const resetBoard = () => {
+    board = []
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < cols; j++) {
+        board[i].push(createCell());
       }
     }
   };
 
-  const putSymbol = (event) => {
-    const element = event.target;
-    if (board[element.row][element.col] !== ''){
-      
+  resetBoard();
+
+  const getBoard = () => {
+    return board;
+  }
+
+  const putSymbol = (row, col, symbol) => {
+    if (board[row][col].getSymbol() !== '') {
+      return false;
     }
+
+    board[row][col].setSymbol(symbol);
+    return true;
   };
 
-  return { renderBoard, putSymbol };
+  const printBoard = () => {
+    const boardWithValues = board.map((row) => {
+      return row.map((cell) => {
+        return cell.getSymbol();
+      });
+    });
+    console.log(boardWithValues);
+  }
+
+  return { getBoard, resetBoard, putSymbol, printBoard };
 })();
 
-const Player = (sym) => {
-  const symbol = sym;
+const Player = (playerName, playerSymbol) => {
+  const name = playerName;
+  const symbol = playerSymbol;
 
-  return { symbol };
+  return { name, symbol };
 };
 
+function createCell() {
+  let symbol = '';
 
-const GameController = (() => {
+  const setSymbol = (inValue) => {
+    symbol = inValue;
+  };
+
+  const getSymbol = () => {
+    return symbol;
+  };
+
+  return { setSymbol, getSymbol };
+}
+
+const GameController = ((playerOneName = "Player One", playerTwoName = "Player Two") => {
+  const winningCombinations = [
+    [[0, 0], [0, 1], [0, 2]], // Top row
+    [[1, 0], [1, 1], [1, 2]], // Middle row
+    [[2, 0], [2, 1], [2, 2]], // Bottom row
+    [[0, 0], [1, 0], [2, 0]], // Left column
+    [[0, 1], [1, 1], [2, 1]], // Middle column
+    [[0, 2], [1, 2], [2, 2]], // Right column
+    [[0, 0], [1, 1], [2, 2]], // Top-left to bottom-right diagonal
+    [[0, 2], [1, 1], [2, 0]], // Top-right to bottom-left diagonal
+  ]
+
+  const checkForWin = () => {
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+
+      const board = Gameboard.getBoard()
+      const firstCell = board[a[0]][a[1]].getSymbol();
+      if (firstCell === '') {
+        continue;
+      }
+
+      if (firstCell === board[b[0]][b[1]].getSymbol() && firstCell === board[c[0]][c[1]].getSymbol()) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   let currentPlayerIndex = 0;
-  const startGame = () => {
-    const players = [Player('X'), Player('O')];
+  let players = [Player(playerOneName, 'X'), Player(playerTwoName, 'O')]
+
+  const switchTurn = () => {
+    currentPlayerIndex = (currentPlayerIndex + 1) % 2;
   };
 
-  const handleTurn = () => {
-
-    currentPlayerIndex += 1;
+  const getActivePlayer = () => {
+    return players[currentPlayerIndex];
   };
 
-  return { startGame, handleTurn };
+  const printNewRound = () => {
+    Gameboard.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const newGame = () => {
+    currentPlayerIndex = 0;
+    Gameboard.resetBoard();
+  };
+
+  const playRound = (row, column) => {
+    if (!Gameboard.putSymbol(row, column, getActivePlayer().symbol)) {
+      return;
+    }
+
+    if (checkForWin()) {
+      console.log(`${getActivePlayer().name} won!`);
+      newGame();
+    }
+
+    switchTurn();
+    printNewRound();
+  };
+
+  printNewRound();
+
+  return { playRound, getActivePlayer, newGame };
 })();
