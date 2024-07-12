46e9db86a0be72e361cf6f87ae8caf6d444c7f1f
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2024-07-12 16:23:47 +0300

    Eslint, I don't know

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+// eslint.config.js
+module.exports = [
+  {
+    rules: {
+      'indent': [
+        'error',
+        2
+      ],
+      'no-unused-vars': 'warn',
+      'semi': 'error',
+      'prefer-const': 'error',
+      'quotes': [
+        'error',
+        'single'
+      ],
+      'no-param-reassign': 'off',
+      'no-plusplus': 'off',
+      'prefer-destructuring': 'off'
+    }
+  }
+];
