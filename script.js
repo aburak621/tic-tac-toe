892f0af927c13dde1712effb7d42ea2d6723828b
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2024-07-13 13:26:40 +0300

    Slightly better visuals

--- script.js
+++ script.js
@@ -104,14 +104,18 @@ const GameController = ((playerOneName = 'Player One', playerTwoName = 'Player T
     }
 
     if (checkForWin()) {
-      Gameboard.printBoard();
       console.log(`${getActivePlayer().name} won!`);
-      newGame();
+      Gameboard.printBoard();
+      setTimeout(() => {
+        newGame();
+      }, 1000);
       return;
     } else if (checkForTie()) {
-      Gameboard.printBoard();
       console.log('It is a tie!');
-      newGame();
+      Gameboard.printBoard();
+      setTimeout(() => {
+        newGame();
+      }, 1000);
       return;
     }
 
